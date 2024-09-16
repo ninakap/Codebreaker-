6b4816616bc08b798e417cfb25de0e2df5ed6465
ninakap <1214412@student.roc-nijmegen.nl>
2024-09-16 14:43:58 +0200

    inpu6

---
+++ input03/src/input03/Input03.java
@@ -0,0 +1,29 @@
+package input03;
+
+
+import codeBreaker.Scanner;
+import codeBreaker.scanner;
+
+class Input03 {
+
+    public static void main(String[] args) {
+        //Create a Scanner
+        
+        //Find and print the sum of three integers entered by the user
+        
+    	java.util.Scanner sc= new java.util.Scanner(System.in);
+        
+int x =  sc.nextInt();
+int y =  sc.nextInt();
+int z = sc.nextInt();
+  
+  System.out.println(x);
+  
+  sc.close();
+  
+        
+	
+        //Remember to close the Scanner
+        
+    }
+}
